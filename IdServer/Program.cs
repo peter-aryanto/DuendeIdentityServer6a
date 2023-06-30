386ebcccc7b72b65c07378c477660fedbd9da0f9
Peter Aryanto <peter.aryanto@gmail.com>
2023-07-01 00:43:25 +1000

    Setup identity server /.well-known/openid-configuration

--- IdServer/Program.cs
+++ IdServer/Program.cs
@@ -1,5 +1,27 @@
+using Duende.IdentityServer.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddIdentityServer()
+  .AddInMemoryApiScopes(
+    new List<ApiScope>
+    {
+      new ApiScope("weatherapi"),
+    }
+  )
+  .AddInMemoryApiResources(
+    new List<ApiResource>
+    {
+      new ApiResource("weatherapi")
+    }
+  )
+  .AddInMemoryClients(new List<Client>{});
+
+
+
 var app = builder.Build();
+
+app.UseIdentityServer();
 
 app.MapGet("/", () => "Hello World!");
 
